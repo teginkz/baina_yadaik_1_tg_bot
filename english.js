121406eb135d5b4d37b3f4eadb32494746d2a7db
MyGoofyKindaBad <speedclanker@gmailc.com>
2023-09-12 12:15:45 +0600

    small bug fix

--- english.js
+++ english.js
@@ -1,11 +1,11 @@
 module.exports = {
     unit1: {
-        mainTitle: 'Unit 1 TestMainTitle',
+        mainTitle: 'unit 1',
         task1: 'unit 1 task 1',
         task2: 'unit 1 task 2'
     },
     unit2: {
-        mainTitle: "Unit 2 TestMainTitle",
+        mainTitle: "unit 2",
         task1: 'unit 2 task 1',
         task2: 'unit 2 task 2'
     }
